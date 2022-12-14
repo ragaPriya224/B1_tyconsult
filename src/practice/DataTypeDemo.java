7503c27ad0a1988ba56fa8df45e236ad3608374d
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-12-14 22:18:50 +0530

    CODE COMMIT

---
+++ src/practice/DataTypeDemo.java
@@ -0,0 +1,37 @@
+package practice;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+//		biscuit cream = 10; 
+//		type variableName = value;
+        byte choclate = 10;
+//        byte -> datatype
+//        choclate -> variable name 
+//        = -> assigning
+//        10 -> value 
+        
+        System.out.println("choclate");
+        System.out.println(choclate);
+        int ice = 10;
+        short burger = 500;
+        long pizza = 20L;
+        float chicken = 50.43f;
+        double subway = 489.8979d;
+        System.out.println(ice);
+
+        char initial = 'a';
+        System.out.println(initial);
+        
+        boolean flag = true;
+        System.out.println(flag);
+        int vegg = 5;
+        vegg = 50;
+        System.out.println(vegg);
+        int mouse;
+        mouse = 100;
+        System.out.println(mouse);
+        
+	}
+
+}
---
+++ src/practice/Demo.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("hi team");
+        System.out.println(3+3);
+	}
+
+}
