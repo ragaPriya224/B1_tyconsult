07c662eaf55b7085ddba26ab17154d2e0eb89c4a
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2023-02-20 20:55:12 +0530

    Add files via upload

---
+++ springcode/qualitasDemo/src/main/java/com/example/qualitasDemo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.qualitasDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "welcome buddy";
+	}
+
+}
---
+++ springcode/qualitasDemo/src/test/java/com/example/qualitasDemo/QualitasDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qualitasDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualitasDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
